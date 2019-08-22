67676a070c7de064ead59d11161a2b5573932724
Souvik50 <duttasouvik916@yahoo.in>
2019-08-22 11:01:58 +0530

    broken the homepage into smaller components and also added some stylings to the menu-items

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './homepage.styles.scss';
+import Directoty from '../../components/directory/directory.component';
+
+const HomePage = () => {
+    return(
+        <div className="homepage">
+            <Directoty/>
+        </div>
+    );
+}
+
+export default HomePage;
